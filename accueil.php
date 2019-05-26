28631ac9ad636537f8597ed94b030e21badc0519
OguzhanUmutOzgur <51048361+OguzhanUmutOzgur@users.noreply.github.com>
2019-05-26 15:56:38 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta charset="utf-8" />
+    <link rel="shortcut icon" type="image/x-icon" href="images/iconhytale.ico" /> <!-- Icon du site --> 
+		<link rel="stylesheet" href="cv.css">                                         
+    <link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+    <script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>   <!-- Le bibliothéque pour affichage des images --> 
+    <script type="text/javascript" src="function.js"></script>
+		<title>Medusa</title>
+</head>
+
+<body>
+	<div id="upside">
+			<img id="upperbackgroundphoto" src="images/hytale.png">  <!-- Logo au-dessus des boutons -->
+
+  <a class="login" href="login.php"> Log In</a>                <!-- Boutons de se connecter--> 
+      <ul>
+  <a class="menü" href="accueil.php">Accueil</a>
+  <a class="menü" href="haberler.php">Galerie</a>                <!-- Les boutons pour acceder aux autres pages --> 
+  <a class="menü" href="iletişim.php">Contact</a>
+  <a class="menü" href="hakkında.php">Qui sommes-nous ? </a>
+      </ul>
+      
+      <div id="body">
+          <iframe id="trailer" src="https://www.youtube.com/embed/O1EAeNdTFHU" 
+          frameborder="0" allow="accelerometer; autoplay; encrypted-media;      
+          gyroscope; picture-in-picture" allowfullscreen></iframe>                 <!-- Le lien du vidéo qui est affiché dans le page d'accueil --> 
+      </div>
+		
+
+
+
+			
+	</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
