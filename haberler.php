fa6d9722d3178094b3b135c88c3688901a65bee9
OguzhanUmutOzgur <51048361+OguzhanUmutOzgur@users.noreply.github.com>
2019-05-26 16:02:51 +0200

    Add files via upload

---
+++ haberler.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta charset="utf-8" />
+		<link rel="shortcut icon" type="image/x-icon" href="images/iconhytale.ico" />
+		<link rel="stylesheet" href="cv.css">
+		<link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+		<script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+    <script type="text/javascript" src="function.js"></script>
+		<title>Medusa</title>
+</head>
+	
+
+<body>
+	<div id="upside">
+
+			<img id="upperbackgroundphoto" src="images/hytale.png">
+  <a class="login" href="login.php"> Log In</a>
+      <ul>
+  <a class="menü" href="accueil.php">Accueil</a>
+  <a class="menü" href="haberler.php">Galerie</a>
+  <a class="menü" href="iletişim.php">Contact</a>
+  <a class="menü" href="hakkında.php">Qui sommes-nous ? </a>
+      </ul>
+      
+      <div class="gallery" id="body">
+      	
+   		<a href="images/2.jpg" data-lightbox="mygallery"><img id="galeri" src="images/2.jpg"></a>  
+      	<a href="images/3.jpg" data-lightbox="mygallery"><img id="galeri" src="images/3.jpg"></a>  
+      	<a href="images/4.jpg" data-lightbox="mygallery"><img id="galeri" src="images/4.jpg"></a>  
+      	<a href="images/5.jpg" data-lightbox="mygallery"><img id="galeri" src="images/5.jpg"></a>  
+      	<a href="images/6.jpg" data-lightbox="mygallery"><img id="galeri2" src="images/6.jpg"></a>  
+      	<a href="images/7.jpg" data-lightbox="mygallery"><img id="galeri2" src="images/7.jpg"></a>  
+      	<a href="images/8.jpg" data-lightbox="mygallery"><img id="galeri2" src="images/8.jpg"></a>  
+      	<a href="images/9.jpg" data-lightbox="mygallery"><img id="galeri2" src="images/9.jpg"></a>  
+      	
+
+      </div>
+		
+
+
+
+			
+	</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
